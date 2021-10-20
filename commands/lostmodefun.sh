7fac1e81eefb83f017b4054ad651b8ab79d2ee56
JCSmillie <jsmillie@gatewayk12.org>
2021-10-20 15:19:52 -0400

    Lost Mode Status
    Added Ability to check lost mode status on a particular device.

--- commands/lostmodefun.sh
+++ commands/lostmodefun.sh
@@ -100,6 +100,98 @@ DisableLostMode(){
 	fi
 }
 
+CheckLostMode() {
+	#Build Query.  Just asking for current data on last beat, lostmode status, and location data if we can get it.
+	content="{\"accessToken\":\"$APIKey\",\"options\":{\"os\":\"ios\",\"serial_numbers\":[\"$DeviceSerialNumber\"],\"specific_columns\":\"deviceudid,date_last_beat,tags,lostmode_status,longitude,latitude\"}}"
+
+	output=$(curl -s -k -X POST -d 'content='$content 'https://managerapi.mosyle.com/v2/listdevices')
+	
+	#echo "Output->> $output"
+
+	if echo "$output" | grep "DEVICES_NOTFOUND"; then
+		log_line "Mosyle doesn't know $DeviceSerialNumber.  Epic Fail."
+		UDID="NOTFOUND"
+
+	#If device is ENABLED	
+	elif echo "$output" | grep "ENABLED"; then 
+		#echo "Lost Mode is enabled."
+		#Parse what was returned.
+		WHATWEGOTBACK=$(echo "$output"| awk 'BEGIN{FS=",";RS="},{"}{print $0}' | perl -pe 's/.*"deviceudid":"?(.*?)"?,"date_last_beat":"?(.*?)"?,"tags":"(.*?)","lostmode_status":"?(.*?)"?,"longitude":"?(.*?)","latitude":"?(.*?)",*.*/\1\t\2\t\3\t\4\t\5\t\6/' | cut -d ']' -f 1)
+		echo "LOST ENABLED--> $WHATWEGOTBACK"
+		unset UDID
+		
+	else
+		#Only enabled state gives us more than we need.  All other states we can go with bare minimum
+		WHATWEGOTBACK=$(echo "$output"| awk 'BEGIN{FS=",";RS="},{"}{print $0}' | perl -pe 's/.*"deviceudid":"?(.*?)"?,"date_last_beat":"?(.*?)"?,"tags":"(.*?)","lostmode_status":"?(.*?)",*.*/\1\t\2\t\3\t\4/' | cut -d ']' -f 1)
+		echo "ALL OTHER STATUSES--> $WHATWEGOTBACK"
+		unset UDID
+
+	fi
+
+	if [ ! "$UDID" = "NOTFOUND" ]; then
+		#Cut that up to variables.
+		UDID=$(echo "$WHATWEGOTBACK" |  cut -f 1 -d$'\t' )
+		LASTBEAT=$(echo "$WHATWEGOTBACK" |  cut -f 2 -d$'\t' )
+		TAGS=$(echo "$WHATWEGOTBACK" | cut -f 3 -d$'\t')
+		LOSTMODE=$(echo "$WHATWEGOTBACK" |  cut -f 4 -d$'\t' )
+		LONGITUDE=$(echo "$WHATWEGOTBACK" |  cut -f 5 -d$'\t' )
+		LATITUDE=$(echo "$WHATWEGOTBACK" |  cut -f 6 -d$'\t' )
+		
+		echo "($UDID) / ($LASTBEAT) / ($TAGS) / ($LOSTMODE) / ($LONGITUDE) / ($LATITUDE)"
+		
+		
+		LASTBEATDATE=$(python -c "import datetime; print(datetime.datetime.fromtimestamp(int("$LASTBEAT")).strftime('%Y-%m-%d %H:%M:%S'))")
+		
+		#Figure out how many hours ago last beat was
+		current_time=$(date +%s)
+		current_time=$(expr "$current_time" / 3600 )
+		before_time=$(expr "$LASTBEAT" / 3600 )
+		hoursago=$(expr "$current_time" - "$before_time" )
+		
+		echo "$current_time"
+		echo "$before_time"
+		echo "$hoursago"
+		
+		
+		echo "$DeviceSerialNumber last seen by Mosyle on $LASTBEATDATE"
+		
+		echo "${Red}--------------------------------------------------${reset}"
+		echo "${Blue}UDID=${Green}$UDID${reset}"
+		echo "${Blue}DeviceSerialNumber=${Green}$DeviceSerialNumber${reset}"
+		echo "${Blue}TAGS=${Green}$TAGS${reset}"
+		echo "${Blue}ASSET TAG=${Green}$ASSETTAG${reset}"
+		echo "${Blue}ENROLLMENT_TYPE=${Green}$ENROLLMENT_TYPE${reset}"
+		echo "${Blue}USERID=${Green}$USERID${reset}"
+		echo "${Blue}ASSIGNED TO=${Green}$NAME${reset}"
+		echo " "
+		echo "${Blue}Last Seen (EPOCH)=${Green}$LASTBEAT${reset}"
+		echo "${Blue}Last Seen (Date)=${Green}$LASTBEATDATE${reset}"	
+		echo "${Blue}Last Seen (Hours Ago)=${Green}$hoursago${reset}"
+		echo "${Blue}Lost Mode Status=${Green}$LOSTMODE${reset}"	
+
+		if [ ! -z "$LONGITUDE" ] && [ ! -z "$LATITUDE" ]; then
+			echo "${Blue}Location Data=${Green}$LATITUDE,$LONGITUDE${reset}"
+			echo " "
+			echo "GO TO THIS LINK TO SEE LOST IPAD LOCATION-> https://maps.google.com/?q=$LATITUDE,$LONGITUDE"
+
+		fi
+		
+		echo "${Red}--------------------------------------------------${reset}"
+		
+		
+
+
+
+	
+		#If TAGS variable says null then swap out variable
+		#to be nothing.
+		if [ "$TAGS" = "null" ] ; then
+			TAGS=""
+		fi
+	fi
+}
+
+
 #############################
 #          Do Work          #
 #############################
@@ -122,6 +214,9 @@ elif [ "$1" = "--enable" ]; then
 	
 elif [ "$1" = "--disable" ]; then
 	DisableLostMode
+	
+elif [ "$1" = "--status" ]; then
+	CheckLostMode	
 else
 	cli_log "Bad arguments given <$1/$2> Try again."
 	exit 1
