6d1d27d8723c1336f2ca8379db771da7e4db9483
JCSmillie <jsmillie@gatewayk12.org>
2022-02-21 17:06:57 -0500

    Intergrate MacOS searches into GetInfo
    Now when running GetInfo we are searching both iPads/iPhones and Macs.
    
    Also added code to check for current MacOS export and do so.  Its not as nice as the parallel run of User and iOS yet... but its functional.

--- commands/getinfo.sh
+++ commands/getinfo.sh
@@ -34,30 +34,52 @@ if [ -z "$1" ]; then
 fi
 
 #Find by Asset Tag, Serial, or Username.  Same Search actually works both ways.
-FoundIt=$(cat "$TEMPOUTPUTFILE_MERGEDIOS" | cut -d$'\t' -f 2-5,7-9 | grep "$1")
+FoundItIOS=$(cat "$TEMPOUTPUTFILE_MERGEDIOS" | cut -d$'\t' -f 2-5,7-9 | grep "$1")
 #Strip FoundIt down to JUST THE SERIAL #
-FoundIt=$(echo "$FoundIt" | cut -d$'\t' -f 1)
-echo "We Got a Hit-> $FoundIt"
-if [ ! -z "$FoundIt" ]; then
-	echo "Tag Check-> $FoundIt"
+FoundItIOS=$(echo "$FoundItIOS" | cut -d$'\t' -f 1)
+if [ "$MB_DEBUG" = "Y" ]; then
+	echo "We Got a Hit (iPad)-> $FoundItIOS"
+fi
+
+#Find by Asset Tag, Serial, or Username.  Same Search actually works both ways.
+FoundItMACOS=$(cat "$TEMPOUTPUTFILE_MERGEDMAC" | cut -d$'\t' -f 2,4-5,7-9 | grep "$1")
+#Strip FoundIt down to JUST THE SERIAL #
+FoundItMACOS=$(echo "$FoundItMACOS" | cut -d$'\t' -f 1)
+if [ "$MB_DEBUG" = "Y" ]; then
+	echo "We Got a Hit (Mac)-> $FoundItMACOS"
+fi
+
+####THIS IS WHERE WE SEARCH FOR IPADS
+if [ ! -z "$FoundItIOS" ]; then
+	if [ "$MB_DEBUG" = "Y" ]; then
+		echo "Tag Check-> $FoundItIOS"
+	fi
+	
+	#Throw up a banner for the iPads section
+	echo "${Red}--------------------------------------------------${reset}"
+	echo "${Red}|==               ${Yellow}iPads/iPhones${Red}                ==|${reset}"
+	echo "${Red}--------------------------------------------------${reset}"
 
 	#Check to see how many results we got.
-	WCC=$(echo "$FoundIt" | wc -l )
+	WCC=$(echo "$FoundItIOS" | wc -l )
 	WCC="${WCC//[[:space:]]/}"
-	echo "WC=$WCC"
+	
+	if [ "$MB_DEBUG" = "Y" ]; then
+		echo "WC=$WCC"
+	fi
 	
 	#If we got more than 1 result lets use a loop to look
 	#each one up.
 	if [ "$WCC" -gt "1" ]; then
 		echo "Search for $1 gave multiple results."
-		
-		echo $FoundIt | while read FoundOne; do
-			
+
+		echo "$FoundItIOS" | while read FoundOne; do
+
 			FoundOne=$(cat "$TEMPOUTPUTFILE_MERGEDIOS" | grep "$FoundOne")
-			
-						
+
+
 			line="$FoundOne"
-			ParseIt_ios 
+			ParseIt_ios
 
 			if [ -z "$ENROLLMENT_TYPE" ]; then
 			cli_log "$1 <$ASSETTAG/$DeviceSerialNumber> Is in Limbo or Shared Mode."
@@ -74,18 +96,16 @@ if [ ! -z "$FoundIt" ]; then
 				echo "${Blue}ASSIGNED TO=${Green}$NAME${reset}"
 				echo "${Red}--------------------------------------------------${reset}"
 			fi
-			
-			
+
+
 		done
-		
-		exit 0
-		
+
 	else
-		
+
 		#Otherwise we got one device.  Just display with no loop call.
-		FoundIt=$(cat "$TEMPOUTPUTFILE_MERGEDIOS" | grep "$FoundIt")
-		line="$FoundIt"
-		ParseIt_ios 
+		FoundItIOS=$(cat "$TEMPOUTPUTFILE_MERGEDIOS" | grep "$FoundItIOS")
+		line="$FoundItIOS"
+		ParseIt_ios
 
 		if [ -z "$ENROLLMENT_TYPE" ]; then
 			cli_log "$1 <$ASSETTAG/$DeviceSerialNumber> Is in Limbo or Shared Mode."
@@ -102,16 +122,91 @@ if [ ! -z "$FoundIt" ]; then
 
 		fi
 	fi
+else
+	cli_log "No hits for $1 in the iPad data.  Please double check your info and try again."
+fi
 
-
+####THIS IS WHERE WE SEARCH FOR MACS
+if [ ! -z "$FoundItMACOS" ]; then
+	if [ "$MB_DEBUG" = "Y" ]; then
+		echo "Tag Check-> $FoundItMACOS"
+	fi
 	
-	#We had a good hit.  Stop.
-	exit 0
+	#Throw up a banner for the Mac section
+	echo "${Red}--------------------------------------------------${reset}"
+	echo "${Red}|==                    ${Yellow}Macs${Red}                    ==|${reset}"
+	echo "${Red}--------------------------------------------------${reset}"
+
+	#Check to see how many results we got.
+	WCC=$(echo "$FoundItMACOS" | wc -l )
+	WCC="${WCC//[[:space:]]/}"
+	if [ "$MB_DEBUG" = "Y" ]; then
+		echo "WC=$WCC"
+	fi
+
+	#If we got more than 1 result lets use a loop to look
+	#each one up.
+	if [ "$WCC" -gt "1" ]; then
+		echo "Search for $1 gave multiple results."
+
+		echo "$FoundItMACOS" | while read FoundOne; do
+
+			FoundOne=$(cat "$TEMPOUTPUTFILE_MERGEDMAC" | grep "$FoundOne")
+
+
+			line="$FoundOne"
+			ParseIt_MacOS			
+
+			if [ -z "$ENROLLMENT_TYPE" ]; then
+			cli_log "$1 <$ASSETTAG/$DeviceSerialNumber> Is in Limbo or Shared Mode."
+			else
+				echo "${Red}--------------------------------------------------${reset}"
+				echo "${Blue}UDID=${Green}$UDID${reset}"
+				echo "${Blue}DeviceSerialNumber=${Green}$DeviceSerialNumber${reset}"
+				echo "${Blue}CURRENTNAME=${Green}$CURRENTNAME${reset}"
+				echo "${Blue}TAGS=${Green}$TAGS${reset}"
+				echo "${Blue}ASSET TAG=${Green}$ASSETTAG${reset}"
+				echo "${Blue}LASTCHECKIN=${Green}$LASTCHECKIN${reset}"
+				echo "${Blue}ENROLLMENT_TYPE=${Green}$ENROLLMENT_TYPE${reset}"
+				echo "${Blue}USERID=${Green}$USERID${reset}"
+				echo "${Blue}ASSIGNED TO=${Green}$NAME${reset}"
+				echo "${Red}--------------------------------------------------${reset}"
+			fi
+
+
+		done
+
+	else
+
+		#Otherwise we got one device.  Just display with no loop call.
+		FoundItMACOS=$(cat "$TEMPOUTPUTFILE_MERGEDMAC" | grep "$FoundItMACOS")
+		line="$FoundItMACOS"
+		ParseIt_MacOS
+
+		if [ -z "$ENROLLMENT_TYPE" ]; then
+			cli_log "$1 <$ASSETTAG/$DeviceSerialNumber> Is in Limbo or Shared Mode."
+		else
+			echo "${Blue}UDID=${Green}$UDID${reset}"
+			echo "${Blue}DeviceSerialNumber=${Green}$DeviceSerialNumber${reset}"
+			echo "${Blue}CURRENTNAME=${Green}$CURRENTNAME${reset}"
+			echo "${Blue}TAGS=${Green}$TAGS${reset}"
+			echo "${Blue}ASSET TAG=${Green}$ASSETTAG${reset}"
+			echo "${Blue}LASTCHECKIN=${Green}$LASTCHECKIN${reset}"
+			echo "${Blue}ENROLLMENT_TYPE=${Green}$ENROLLMENT_TYPE${reset}"
+			echo "${Blue}USERID=${Green}$USERID${reset}"
+			echo "${Blue}ASSIGNED TO=${Green}$NAME${reset}"
+
+		fi
+	fi
 fi
 
 ###################
-# If we are here then we got no hits.
-cli_log "No hits for $1.  Please double check your info and try again."
-exit 1
-
+#If both results came back negative
+if [ -z "$FoundItIOS" ] && [ -z "$FoundItMACOS" ]; then
+	cli_log "No hits for $1.  Please double check your info and try again."
+	exit 1
+	
+else
+	exit 0
+fi
 
